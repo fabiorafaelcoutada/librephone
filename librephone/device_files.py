502abd9b30923dd4e8278514cb17f9634cf63e57
Rob Savoye <rob@fsf.org>
2025-10-17 13:48:51 -0600

    fix: Start adding support to extract the architecure of the binary

--- librephone/device_files.py
+++ librephone/device_files.py
@@ -27,8 +27,10 @@ from pathlib import Path
 import re
 import json
 import hashlib
+import magic
 from progress.spinner import Spinner
-from librephone.typedefs import Cputypes, Gpumodels, Devstatus, Imgtypes, Bintypes, Archtypes, Celltypes, Nettypes, Wifitypes, Filetypes, Blobtypes
+import subprocess
+from librephone.typedefs import Gpumodels, Devstatus, Imgtypes, Bintypes, Archtypes, Celltypes, Nettypes, Wifitypes, Filetypes, Blobtypes
 
 import librephone as pt
 rootdir = pt.__path__[0]
@@ -48,18 +50,6 @@ class DeviceFiles(object):
         self.files = dict()
         # FIXME: drop .pb and .dat for now, less clutter
         self.keep = (".hex", ".img", ".bin", ".fw", ".fw2")
-        self.suffixes = {".jar": "JAR",
-                    ".apk": "APK",
-                    ".so": "LIBRARY",
-                    ".sh": "BASH",
-                    ".img": "IMAGE",
-                    ".py": "PYTHON",
-                    ".bin": "BIN",
-                    ".hex": "HEX",
-                    ".pb": "UNKNOWN",
-                    ".fw": "FIRMWARE",
-                    ".fw2": "FIRMWARE",
-                    }
 
         # These are the values returned from using the command line
         # file program
@@ -115,6 +105,38 @@ class DeviceFiles(object):
         # log.debug(f"Extracting md5sum for file {file}")
         md5sum = hashlib.md5(open(path,'rb').read()).hexdigest()
 
+        # result = subprocess.run(
+        #     [
+        #         "objdump",
+        #         "-a",
+        #         path,
+        #     ],
+        #     capture_output=True,
+        # )
+        result = magic.from_file(path)
+        print(f"MAGIC: {filespec} is {result}")
+        # if str(result.stderr).find("file format not recognized") > 0:
+        #     pass
+        pat = "(ARM|QUALCOMM|MIPS|RISC-V)[ a-zA-Z-]*,"
+        archtype = {"ARM": Archtypes.ARM,
+                    "ARM aarch64": Archtypes.AARCH64,
+                    "QUALCOMM DSP6": Archtypes.QUALCOMM,
+                    "UNKNOWN": Archtypes.UNKNOWN,
+                    "RISC-V": Archtypes.RISCV,
+                    "ARM Cortex-M firmware": Archtypes.CORTEXM,
+                    # FIXME need to add risc-v and mips
+                    }
+        out = str(result)
+        match = re.search(pat, out)
+        if match:
+            # breakpoint()
+            ftype = out[match.start():match.end() - 1]
+            arch = archtype[ftype].value
+            print(f"FIXME: {filespec} is {arch}")
+        else:
+            arch =  Archtypes(Archtypes.UNKNOWN).value
+        #print(f"FIXME: {filespec} is {archtype[ftype]}")
+
         # file_type = self.get_filetype(path)
         file_type = self.get_magic(filespec).value
         #if file_type not in self.files:
@@ -126,7 +148,9 @@ class DeviceFiles(object):
                     "path": os.path.dirname(path),
                     "version": 22.2, # FIXME: this shouldn't be hardcoded
                     }
-        print(metadata)
+        if arch != "UNKNOWN":
+            metadata["arch"] = arch
+        # print(metadata)
         return metadata
 
     def get_magic(self,
@@ -170,6 +194,7 @@ class DeviceFiles(object):
                      {"pat": "misound.*.bin", "type": Bintypes.RTPSTREAM},
                      {"pat": "config.bin", "type": Bintypes.CONFIG},
                      {"pat": "[0-9]*_pre.bin", "type": Bintypes.CAMERA},
+                     {"pat": "iris.*.fw", "type": Bintypes.CAMERA},
                      {"pat": "CFR_OnePlus.*.bin", "type": Bintypes.CAMERA},
                      {"pat": "_FT3518_.*.bin", "type": Bintypes.TOUCHSCREEN2},
                      {"pat": "_FT3681_.*.bin", "type": Bintypes.TOUCHSCREEN2},
@@ -190,12 +215,14 @@ class DeviceFiles(object):
                      {"pat": "_S6SY791_.*.img", "type": Bintypes.TOUCHSCREEN},
                      {"pat": "_S6SY792_.*.bin", "type": Bintypes.TOUCHSCREEN},
                      {"pat": "_S6SY792_.*.img", "type": Bintypes.TOUCHSCREEN},
-                     {"pat": "_GT9886_SAMSUNG.*.img", "type": Bintypes.TOUCHSCREEN},
+                     {"pat": "anc_.*.bin", "type": Bintypes.TOUCHSCREEN},
+                     {"pat": "_GT98[0-9]*_SAMSUNG.*.img", "type": Bintypes.TOUCHSCREEN},
                      {"pat": "goodix_firmware_ak.*.bin", "type": Bintypes.TOUCHSCREEN},
                      {"pat": "cs40l26.*.bin", "type": Bintypes.TOUCHSCREEN},
                      {"pat": "aoc.bin", "type": Bintypes.TOUCHSCREEN},
                      {"pat": "shader_PROGRAM.*.bin", "type": Bintypes.SHADER},
                      {"pat": "a630_sqe.*.bin", "type": Bintypes.GPU},
+                     {"pat": "a630_sqe.*.fw", "type": Bintypes.GPU},
                      {"pat": "mali_csffw-.*.bin", "type": Bintypes.GPU},
                      {"pat": "bwlan.bin", "type": Bintypes.WIFI},
                      {"pat": "bdwlan.bin", "type": Bintypes.WIFI},
@@ -219,7 +246,13 @@ class DeviceFiles(object):
                      {"pat": "yyz.bin", "type": Bintypes.CAMERA},
                      {"pat": "zc.bin", "type": Bintypes.CAMERA},
                      {"pat": "a530.*.fw", "type": Bintypes.WIFI_BLUETOOTH},
+                     {"pat": "amss20.bin", "type": Bintypes.WIFI_BLUETOOTH},
+                     {"pat": "m3.bin", "type": Bintypes.WIFI_BLUETOOTH},
+                     {"pat": "bdwlan.*", "type": Bintypes.WIFI_BLUETOOTH},
+                     {"pat": "regdb.bin", "type": Bintypes.WIFI_BLUETOOTH},
                      {"pat": "fw_bcmdhd.bin", "type": Bintypes.WIFI_BLUETOOTH},
+                     {"pat": "htnv[0-9]*.bin", "type": Bintypes.BLUETOOTH},
+                     {"pat": "htbtfw[0-9]*.bin", "type": Bintypes.BLUETOOTH},
                      {"pat": "a540.*.fw", "type": Bintypes.GPU},
                      {"pat": "a[0-9]*_gmu.bin", "type": Bintypes.GPU},
                      {"pat": "a[0-9]*]_sqe.bin", "type": Bintypes.GPU},
@@ -244,18 +277,61 @@ class DeviceFiles(object):
                      {"pat": "effect_[0-9].bin", "type": Bintypes.VIBRATION},
                      {"pat": "w_dual_calibration.bin", "type": Bintypes.CAMERA},
                     )
-        print(f"FIXME: {filespec}")
+        # print(f"FIXME: {filespec}")
         for name in nametypes:
             pat = re.compile(name["pat"])
             if re.search(pat, filespec):
                 return name["type"]
+
+        # These are the top level blobs from the radio directory, and need to be
+        # direct name match.
+        name = os.path.basename(filespec)
+        radioblobs = {"xbl.img": Bintypes.UEFIBOOT,
+                      "abl.img": Bintypes.UEFIBOOT,
+                      "boot.img": Bintypes.BOOT,
+                      "init_boot.img": Bintypes.BOOT,
+                      "recovery.img": Bintypes.BOOT,
+                      "dtbo.img": Bintypes.DTB,
+                      "bl1.img": Bintypes.BOOT,
+                      "bl31.img": Bintypes.BOOT,
+                      "gcf.img": Bintypes.UNKNOWN,
+                      "gsa.img": Bintypes.SECURITY,
+                      "pbl.img": Bintypes.UNKNOWN,
+                      "ldfw.img": Bintypes.UNKNOWN,
+                      "vbmeta.img": Bintypes.AVB,
+                      "vbmeta_system.img": Bintypes.AVB,
+                      "vbmeta_boot.img": Bintypes.AVB,
+                      "pvmfw.img": Bintypes.SECURITY,
+                      "tzsw.img": Bintypes.BOOT,
+                      "tz.img": Bintypes.BOOT,
+                      "gsa_bl1.img": Bintypes.SECURITY,
+                      "modem.img": Bintypes.CELL_WIFI_GPS_BLUETOOTH,
+                      "bluetooth.img": Bintypes.BLUETOOTH,
+                      "studybk.img": Bintypes.UNKNOWN,
+                      "uefisecapp.img": Bintypes.UNKNOWN,
+                      "cpucp.img": Bintypes.UNKNOWN,
+                      "keymaster.img": Bintypes.SECURITY,
+                      "featenabler.img": Bintypes.UNKNOWN,
+                      "devcfg.img": Bintypes.UNKNOWN,
+                      "aop.img": Bintypes.UNKNOWN,
+                      "aoc.img": Bintypes.UNKNOWN,
+                      "hyp.img": Bintypes.SECUREBOOT,
+                      "devcfg.img": Bintypes.UNKNOWN,
+                      "multiimgoem.img": Bintypes.UNKNOWN,
+                      "imagefv.img": Bintypes.GRAPHIC,
+                      "logo.img": Bintypes.GRAPHIC,
+                      }
+
+        # breakpoint()
+        if name in radioblobs:
+            return radioblobs[name]
 
         log.debug(f"No type found for {filespec}, checking magic number")
 
         # FIXME: Limit magic numbers to 4 bytes unless we can figure out
         # a better way to handle different length magic numbers.
         magic_numbers = {'png': bytes([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]),
-                         'AVB0': bytes([0x41, 0x56, 0x42, 0x30]),
+                         'AVB': bytes([0x41, 0x56, 0x42, 0x30]),
                          'ELF64': bytes([0x7f, 0x45, 0x4c, 0x46]),
                          'MSDOS': bytes([0xeb, 0x3c, 0x90, 0x4d]),
                          'BOOT': bytes([0x41, 0x4e, 0x44, 0x52]),
@@ -267,7 +343,7 @@ class DeviceFiles(object):
                          'FIRMWARE': bytes([0x00, 0x00, 0x00, 0xff]),
                          'FIRMWARE1': bytes([0x00, 0x00, 0x20, 0x00]),
                          'FIRMWARE2': bytes([0x03, 0x01, 0x00, 0x00]),
-                         'AUDIOAMP2': bytes([0x08, 0xf9, 0x15, 0x0a]),
+                         # 'AUDIOAMP2': bytes([0x08, 0xf9, 0x15, 0x0a]),
                          'NFC': bytes([0x13, 0x04, 0x98, 0x81]),
                          'FIRMWARE5': bytes([0x61, 0x6e, 0x63, 0x5f]),
                          'FIRMWARE6': bytes([0x69, 0x05, 0x00, 0x00]),
@@ -286,11 +362,12 @@ class DeviceFiles(object):
                          'WIFI': bytes([0x03, 0x46, 0x04, 0x00]),
                          }
 
-        # breakpoint()
+        breakpoint()
+        # foo = magic.from_file(filespec)
         with open(filespec, "rb") as file:
-            magic = file.read(4)
+            magicnum = file.read(4)
             for ftype, num in magic_numbers.items():
-                if num == magic:
+                if num == magicnum:
                     log.debug(f"{filespec} is {ftype}")
                     return Bintypes(ftype)
         return Bintypes.UNKNOWN
