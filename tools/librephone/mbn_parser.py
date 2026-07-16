1856d6f237f3089b0d1f61ce28cc9fab96d60978
FrossGus <lu2jgp@gmail.com>
2026-07-16 21:20:26 +0200

    Add AGPLv3 copyright headers to all tools

--- tools/librephone/mbn_parser.py
+++ tools/librephone/mbn_parser.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+
+# Copyright (c) 2026 Free Software Foundation, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """Parser de archivos MBN/MDT en formato PIL (Qualcomm Peripheral Image Loader).
 
 Analiza headers ELF32/ELF64, tabla de segmentos, entry points,
--- tools/librephone/tlv_decoder.py
+++ tools/librephone/tlv_decoder.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2026 Free Software Foundation, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """TLV decoder for WCN6750 hardware descriptors.
 
 Parses the fw-api/hw/qca6750/v1/tlv_tag_def.h definitions and provides
